6e5ed7b491156e056cda3f1ed8b563fc57b4e71a
joão victor <125697762+sxmyy@users.noreply.github.com>
2023-04-16 23:39:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.jinfrase', {
+    rotate: {x:0, y:80, z:0},
+    duration: 2000
+});
+
+sr.reveal('.jinfrase2', {
+    rotate: {x:0, y:80, z:0},
+    duration: 2500
+});
+
+sr.reveal('.jinfrase3', {
+    rotate: {x:0, y:80, z:0},
+    duration: 3000
+});
+
+sr.reveal('.jinfrase4', {
+    rotate: {x:0, y:80, z:0},
+    duration: 3500
+});
+
+sr.reveal('.ghostprologotitulo', {
+    rotate: {x:0, y:80, z:0},
+    duration: 1500
+});
+
+sr.reveal('.ghostprologotitulo2', {
+    rotate: {x:0, y:80, z:0},
+    duration: 1500
+});
+
+sr.reveal('.ghostprologotexto1', {
+    rotate: {x:0, y:80, z:0},
+    duration: 2500
+})
+
+sr.reveal('.ghostprologotexto2', {
+    rotate: {x:0, y:80, z:0},
+    duration: 2500
+})
+
+sr.reveal('.ghostprologotexto3', {
+    rotate: {x:0, y:80, z:0},
+    duration: 2500
+})
